d4c1bad0c5c24c557f8fec48b7e876b87f2e9779
Arpan-V <arpan24622@gmail.com>
2026-05-17 08:29:54 +0530

    update securityconfig

--- src/main/java/com/arpan/backend/config/SecurityConfig.java
+++ src/main/java/com/arpan/backend/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.arpan.backend.config;
 
 import com.arpan.backend.filter.JwtFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Value;
@@ -112,6 +113,14 @@ public class SecurityConfig {
                 )
 
                 .authenticationProvider(authenticationProvider())
+
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json");
+                            response.getWriter().write("{\"error\":\"Unauthorized\"}");
+                        })
+                )
 
                 .addFilterBefore(
                         jwtFilter,
